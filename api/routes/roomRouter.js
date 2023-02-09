6c62bf9dadc9afbb7b4ed4e3b7159da8da3ffd88
seuungkei <seuungkei@gmail.com>
2023-02-09 18:40:27 +0900

    [41기 김승기] ADD : 위시리스트 페이지 기능 구현

--- api/routes/roomRouter.js
+++ api/routes/roomRouter.js
@@ -1,8 +1,13 @@
 const express = require('express');
 
 const roomController = require('../controllers/roomController');
+const { validationToken } = require('../util/auth');
 
 const router = express.Router();
+
+router.get('/wishList', validationToken, roomController.getMyWishList);
+router.post('/wishList', validationToken, roomController.addRoomToWishList);
+router.delete('/wishList', validationToken, roomController.deleteWishList);
 
 router.get('/', roomController.getRooms);
 
