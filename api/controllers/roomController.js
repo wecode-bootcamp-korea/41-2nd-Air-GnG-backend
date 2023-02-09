1629c92bd0020d975406dff63f0afd4da09f4f81
seuungkei <seuungkei@gmail.com>
2023-02-09 14:07:14 +0900

    [41기 김승기] ADD : 상세 페이지 기능 구현

--- api/controllers/roomController.js
+++ api/controllers/roomController.js
@@ -8,6 +8,13 @@ const getRooms = catchAsync(async (req, res) => {
   return res.status(200).json(result);
 });
 
+const getRoomByDetail = catchAsync(async (req, res) => {
+  const { roomId } = req.params;
+  const room = await roomService.getRoomByDetail(roomId);
+  return res.status(200).json(room);
+});
+
 module.exports = {
   getRooms,
+  getRoomByDetail,
 };
